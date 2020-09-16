aaf9cb2c33e565baeff120efaab4c3b47cda2eb0
epos95 <max.agnesund95@gmail.com>
2020-09-16 14:16:50 +0200

    Added new help messages

--- src/helper.rs
+++ src/helper.rs
@@ -7,10 +7,10 @@ pub enum HelpTypes {
 pub fn show_help(c: HelpTypes) {
     match c {
         HelpTypes::Program => {
-            println!("This is the general help for this program");
+            println!("Byggis\n\nbyggis new (name of problem)\n\tCreate a directory hosting the kattis problems\n\nbyggis run\n\tRun tests\n");
         },
         HelpTypes::New => {
-            println!("Create a new directory for solving kattis problems");
+            println!("Create a new directory for solving kattis problems\nSyntax: \n\tbyggis new name");
         }
     }
 }
